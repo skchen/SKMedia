c06755578710e734e75111d053e086e8774f0971
Shin-Kai Chen <skchen@sixnology.com>
2016-04-19 14:11:48 +0800

    Add iOS project files

---
+++ SKMedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKMedia
+//
+//  Created by Shin-Kai Chen on 2016/4/19.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
